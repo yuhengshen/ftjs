36e519f974dee918d013df3319144dea74dfc7fb
yuhengshen <yuhengshen@qq.com>
2025-02-21 16:42:19 +0800

    chore: release tf-elementv0.0.2-beta.1

---
+++ packages/tf-element/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import dts from "vite-plugin-dts";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      exclude: ["vite.config.ts"],
+    }),
+  ],
+  build: {
+    emptyOutDir: true,
+    minify: true,
+    lib: {
+      entry: "./src/index.ts",
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["vue", "tf-core"],
+    },
+  },
+});

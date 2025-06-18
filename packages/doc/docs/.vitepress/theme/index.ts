0703e3398dc183905fdef8471cad3e4d63f4ee01
yuhengshen <yuhengshen@qq.com>
2025-06-18 10:46:45 +0800

    docs: 增加暗黑模式支持

--- packages/doc/docs/.vitepress/theme/index.ts
+++ packages/doc/docs/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 import DefaultTheme from "vitepress/theme";
-import type { Theme } from "vitepress";
+import { useData, type Theme } from "vitepress";
 import pcUi from "vxe-pc-ui";
 import table from "vxe-table";
 import "vxe-table/lib/style.css";
@@ -7,8 +7,9 @@ import "vxe-pc-ui/lib/style.css";
 import "tdesign-vue-next/es/style/index.css";
 import { ConfigProvider } from "ant-design-vue";
 import zhCN from "ant-design-vue/lib/locale/zh_CN";
-import { h } from "vue";
+import { h, watchEffect } from "vue";
 import { enhanceAppWithTabs } from "vitepress-plugin-tabs/client";
+import { theme as antTheme } from "ant-design-vue";
 
 // 导入自定义样式
 import "./custom.css";
@@ -20,9 +21,29 @@ export default {
     enhanceAppWithTabs(ctx.app);
   },
   Layout: () => {
+    const { isDark } = useData();
+
+    watchEffect(() => {
+      // 设置 VXE-PC-UI 主题
+      pcUi.setTheme(isDark.value ? "dark" : "light");
+      // tdesign-vue-next
+      if (isDark.value) {
+        document.documentElement.setAttribute("theme-mode", "dark");
+      } else {
+        document.documentElement.removeAttribute("theme-mode");
+      }
+    });
+
     return h(
       ConfigProvider,
-      { locale: zhCN },
+      {
+        locale: zhCN,
+        theme: {
+          algorithm: isDark.value
+            ? antTheme.darkAlgorithm
+            : antTheme.defaultAlgorithm,
+        },
+      },
       { default: () => h(DefaultTheme.Layout) },
     );
   },
